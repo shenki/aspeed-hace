4aa9b2f3dbe528809e534d951073b56e9dfb195d
Joel Stanley <joel@jms.id.au>
2021-03-03 16:38:15 +1030

    Map all of io space, cleanups
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define HACE_BASE 0x1E6D0000
+#define ASPEED_IO_BASE 0x10000000
+#define ASPEED_IO_SZ   0x0f000000
 
 void hw_sha1(const unsigned char *pbuf, unsigned int buf_len,
 	       unsigned char *pout, unsigned int chunk_size);
@@ -21,55 +22,44 @@ static void *map;
 
 uint32_t readl(uint32_t addr)
 {
-	if ((addr & 0xFFFF0000) != HACE_BASE) {
-		printf("Ingoring read at %08x\n", addr);
-		return 0;
-	}
-
-	return *(uint32_t *)(map + (addr & 0xFFFF));
+	return *(uint32_t *)(map + (addr & 0x0FFFFFFF));
 }
 
 void writel(uint32_t val, uint32_t addr)
 {
-	if ((addr & 0xFFFF0000) != HACE_BASE) {
-		printf("Ingoring write at %08x=%08x\n", addr, val);
-		return;
-	}
-
-	*(uint32_t *)(map + (addr & 0xFFFF)) = val;
+	*(uint32_t *)(map + (addr & 0x0FFFFFFF)) = val;
 }
 
 
 static const uint8_t test_input[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF };
-static uint8_t test_output[64];
 
 int main()
 {
 	int i;
 	int fd = open("/dev/mem", O_RDWR | O_SYNC);
-	uint8_t *input;
+	uint8_t *buf;
 
-	// map the hace registers
-	map = mmap(NULL, 0x1000,
+	// map the io registers
+	map = mmap(NULL, ASPEED_IO_SZ,
 			PROT_READ | PROT_WRITE,
 			MAP_SHARED,
-			fd, HACE_BASE);
+			fd, ASPEED_IO_BASE);
 	if (map == MAP_FAILED)
 		errx(1, "map failed");
 
 	// do bad things to dram
-	input = mmap(NULL, 0x2000,
+	buf = mmap(NULL, 0x2000,
 			PROT_READ | PROT_WRITE,
 			MAP_SHARED,
 			fd, 0x80000000);
-	if (input == MAP_FAILED)
+	if (buf == MAP_FAILED)
 		errx(1, "map failed");
-	memcpy(input, test_input, sizeof(test_input));
+	memcpy(buf, test_input, sizeof(test_input));
 
-	hw_sha512(0x80000000, sizeof(test_input), 0x80001000, 1024);
+	hw_sha512((void *)0x80000000, sizeof(test_input), (void *)0x80001000, 1024);
 
 	for (i = 0; i < 64; i++) {
-		printf("%02x", input[0x1000+i]);
+		printf("%02x", buf[0x1000+i]);
 	}
 	puts("\n");
 
